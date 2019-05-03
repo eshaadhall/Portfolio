33ff39685240591a976b433eae05485c038d4259
Eshaa Dhall <dhalleshaa@gmail.com>
2019-05-03 11:37:51 -0400

    read me

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Portfolio
+
+This is my portfolio website for Full stack javascript developer.
+
+I have used Node JS, Express, CSS, HTML, Bootstrap and Pug to build this website.
+
+Extra Credit:
+Page not found error page displays the error.message, error.status and error.stack in the error.pug file under views folder.
+
+Customized the style of the css of the home page and the error 404-not found page.
+
+
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { data } = require('./data/data.json');
+const { projects } = data;
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+// to setup pug middleware
+app.set('view engine', 'pug');
+// static route to serve the static files located in the public folder
+app.use('/static',express.static('public'));
+
+app.get('/', (req,res)=>{
+   // const pname = projects[0].name;
+   res.render('index.pug',{projects});
+});
+
+app.get('/about', (req,res)=>{
+   res.render('about.pug');
+});
+
+app.get('/project/:id', (req,res)=>{
+   const { id } = req.params;
+   const title = projects[id].name;
+   const description = projects[id].description;
+   const technologies = projects[id].technologies;
+   const images = projects[id].image_urls;
+   const liveLink = projects[id].live_link;
+   const gitLink = projects[id].github_link;
+   res.render('project.pug', { id, title, description, technologies, images, liveLink, gitLink });
+});
+
+// Middleware to handle 404 Error
+app.use((req, res, next)=> {
+   const err = new Error('The page you are looking for is not available');
+   err.status = 404;
+   next(err);
+});
+
+//Error Handler
+app.use((err, req, res, next)=>{
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+// setup the server
+app.listen(3001, ()=>{console.log('app is running on port 3001')});
